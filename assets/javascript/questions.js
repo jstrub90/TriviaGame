5c71bad6b69349094afec7e98e9a80440dc7b73d
J.Strub <jstrub90@gmail.com>
2019-04-21 22:51:07 -0400

    added questions in js file

---
+++ assets/javascript/questions.js
@@ -0,0 +1,61 @@
+const questions = [
+  {
+    q: "Which one of these was not a beach landing site in the Invasion of Normandy?",
+    c: ["Gold", "Juno", "Sword", "Silver"],
+    a: "Silver"
+  },
+  
+  {
+    q: "King Henry VIII was the second monarch of which European royal house?",
+    c: ["York", "Lancaster", "Tudor", "Stuart"],
+    a: "Tudor"
+  },
+
+  {
+    q: "In what year did the Wall Street Crash take place?",
+    c: ["1929", "1932", "1925", "1930"],
+    a: "1929"
+  },
+
+  {
+    q: "Which one of these tanks was designed and operated by the United Kingdom?",
+    c: ["Tog II", "M4 Sherman", "Tiger H1", "T-34"],
+    a: "Tog II"
+  },
+
+  {
+    q: "Who led the Communist Revolution of Russia?",
+    c: ["Vladmir Lenin", "Joseph Stalin", "Vladmir Putin", "Mikhail Gorbachev"],
+    a: "Vladmir Lenin"
+  },
+
+  {
+    q: "The original Roman alphabet lacked the following letters EXCEPT:",
+    c: ["X", "W", "U", "J"],
+    a: "X"
+  },
+
+  {
+    q: "Which modern day country is the region that was known as Phrygia in ancient times?",
+    c: ["Turkey", "Syria", "Greece", "Egypt"],
+    a: "Turkey"
+  },
+
+  {
+    q: "Who was the first president of the United States?",
+    c: ["George Washington", "James Madison", "Thomas Jefferson", "James K. Polk"],
+    a: "George Washington"
+  },
+
+  {
+    q: "To what political party did Abraham Lincoln belong when elected POTUS?",
+    c: ["Republican", "Democrat", "Independant", "Whig"],
+    a: "Republican"
+  },
+
+  {
+    q: "What year did World War I begin?",
+    c: ["1914", "1905", "1925", "1919"],
+    a: "1914"
+  },
+]
